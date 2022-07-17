d492b5dc5b1795bc7c63b197584016c9d096f0a5
george kavvas <il_gato_nero@hotmail.com>
2022-07-17 18:01:07 +0300

    Add patch method

--- app.js
+++ app.js
@@ -87,6 +87,20 @@ app.route("/articles/:articleTitle")
       }
     }
   );
+})
+
+.patch(function(req, res) {
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {$set: req.body},
+    function(err) {
+      if (!err) {
+        res.send("Successfully updated");
+      } else {
+        res.send(err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
