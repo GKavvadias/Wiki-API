ffdb41889d95af09588afe584c11c0dd1b25b4e4
george kavvas <il_gato_nero@hotmail.com>
2022-07-17 17:30:44 +0300

    Add put method

--- app.js
+++ app.js
@@ -72,6 +72,21 @@ app.route("/articles/:articleTitle")
       res.send("No matching articles");
     }
   });
+})
+
+.put(function(req, res) {
+  Article.replaceOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    {overwrite: true},
+    function(err) {
+      if (!err) {
+        res.send("Successfully updated");
+      } else {
+        res.send("Error is: " + err);
+      }
+    }
+  );
 });
 
 app.listen(3000, function() {
